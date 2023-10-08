5807308a634e05ee38c22c6cd1ff2f3e09a93aff
HAYDEE SHANE SAGUID <2211010@slu.edu.ph>
2023-10-08 14:38:02 +0800

    Modified the Header and Nav

---
+++ script.js
@@ -0,0 +1,50 @@
+// Replace 'YOUR_API_KEY' with your actual Spoonacular API key
+const apiKey = '71b69fc73b0248edb265c0ec9bcc7ad3';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const searchInput = document.querySelector('.searchInput');
+    const recipeResults = document.querySelector('.recipeResults');
+
+    searchInput.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault(); // Prevent the default form submission
+            const query = searchInput.value;
+            if (query.trim() !== '') {
+                fetchRecipes(query);
+            }
+        }
+    });
+
+    async function fetchRecipes(query) {
+        const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&number=10&apiKey=${apiKey}`;
+
+        try {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            displayRecipes(data.results);
+        } catch (error) {
+            console.error('Error fetching recipes:', error);
+        }
+    }
+
+    function displayRecipes(recipes) {
+        recipeResults.innerHTML = ''; // Clear previous results
+
+        recipes.forEach((recipe) => {
+            const recipeCard = document.createElement('div');
+            recipeCard.classList.add('recipeCard');
+
+            const recipeImage = document.createElement('img');
+            recipeImage.src = recipe.image;
+            recipeImage.alt = recipe.title;
+
+            const recipeName = document.createElement('p');
+            recipeName.textContent = recipe.title;
+
+            recipeCard.appendChild(recipeImage);
+            recipeCard.appendChild(recipeName);
+            recipeResults.appendChild(recipeCard);
+        });
+    }
+});
+
