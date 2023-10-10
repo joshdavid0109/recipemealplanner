82c7b69df00714f1b2a21dff2d476c94853b3dd9
2211261 <2211261@slu.edu.ph>
2023-10-10 21:44:43 +0800

    🥧

--- script.js
+++ script.js
@@ -106,57 +106,56 @@ document.addEventListener('DOMContentLoaded', () => {
     function drawNutritionChart(recipeId, nutrients) {
         const labels = nutrients.map((nutrient) => nutrient.name);
         const values = nutrients.map((nutrient) => nutrient.amount);
-       
+    
         const ctx = document.getElementById(`nutritionChart-${recipeId}`).getContext('2d');
-       
+    
         new Chart(ctx, {
-        type: 'pie',
-        data: {
-        labels: labels,
-        datasets: [{
-        data: values,
-        backgroundColor: [
-        'rgba(255, 99, 132, 0.7)',
-        'rgba(54, 162, 235, 0.7)',
-        'rgba(255, 206, 86, 0.7)',
-        'rgba(75, 192, 192, 0.7)',
-        'rgba(153, 102, 255, 0.7)',
-        'rgba(255, 159, 64, 0.7)',
-        // Add more colors as needed
-        ],
-        borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-        // Add more colors as needed
-        ],
-        borderWidth: 1
-        }]
-        },
-        options: {
-        legend: {
-        display: false,
-        },
-        plugins: {
-            tooltip: {
-            enabled: true,
-            displayColors: false,
-            callbacks: {
-            label: function (context) {
-            var label = context.label || '';
-            if (label) {
-            label += ': ';
-            }
-            label += Math.round(context.parsed * 100) / 100;
-            return label;
+            type: 'pie',
+            data: {
+                labels: labels,
+                datasets: [{
+                    data: values,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.7)',
+                        'rgba(54, 162, 235, 0.7)',
+                        'rgba(255, 206, 86, 0.7)',
+                        'rgba(75, 192, 192, 0.7)',
+                        'rgba(153, 102, 255, 0.7)',
+                        'rgba(255, 159, 64, 0.7)',
+                        'rgba(255, 0, 0, 0.7)', 
+                        'rgba(0, 255, 0, 0.7)', 
+                        'rgba(0, 0, 255, 0.7)', 
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(255, 0, 0, 1)', 
+                        'rgba(0, 255, 0, 1)', 
+                        'rgba(0, 0, 255, 1)', 
+                    ],
+                    borderWidth: 1
+                }]
             },
-            }
-            }
+            options: {
+                plugins: {
+                    legend: {
+                        display: false, 
+                    },
+                    tooltip: {
+                        enabled: true,
+                        displayColors: false,
+                        callbacks: {
+                            label: function (context) {
+                                return context.label + ': ' + Math.round(context.parsed * 100) / 100;
+                            },
+                        }
+                    },
+                },
             },
-        },
         });
-       }
-});
+    }
+});// end of document.addEventListener
